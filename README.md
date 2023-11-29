be2c1353a96564e1aa80c3e48bf10d1cd16c0fdf
engalar <engalar@gmail.com>
2023-11-29 18:38:55 +0800

    wip

---
+++ README.md
@@ -0,0 +1,3 @@
+```sh
+docker run -it ghcr.io/puppeteer/puppeteer:21 bash
+```
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+// copy from service
+import formatSize from "pretty-bytes";
+
+import { createBrowser } from "./service/components/browser.js";
+import { createRequestAnalyzer } from "./service/components/request-analyzer.js";
+import { createDocumentGenerator } from "./service/components/document-generator.js";
+import { createModuleConnector } from "./service/components/module-connector.js";
+import { logLevel, logMessage } from "./service/components/logging.js";
+
+const maxDocumentSize = 25000000; // 25 MB
+
+const requestAnalyzer = enableMetrics ? createRequestAnalyzer() : undefined;
+const moduleConnector = createModuleConnector(maxDocumentSize);
+
+// end
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
 
-app.post("/executeCloudService", (req, res) => {
+app.post("/executeCloudService", async (req, res) => {
     const {
         requestId,
         securityToken,
-        timezoneId,
+        timezone,
         timeout,
         applicationUrl,
         generatePath,
@@ -26,7 +41,7 @@ app.post("/executeCloudService", (req, res) => {
     console.log("Received parameters:");
     console.log("Request ID:", requestId);
     console.log("Security Token:", securityToken);
-    console.log("Timezone ID:", timezoneId);
+    console.log("Timezone:", timezone);
     console.log("Timeout:", timeout);
     console.log("Application URL:", applicationUrl);
     console.log("Generate Path:", generatePath);
@@ -38,7 +53,70 @@ app.post("/executeCloudService", (req, res) => {
     console.log("Runtime Version:", runtimeVersion);
     console.log("Project ID:", projectId);
 
-    // You can add the logic to process the parameters and send a response here
+    // params mapping
+    const appUrl = applicationUrl,
+        enableMetrics = false;
+
+    // copy from service
+    await withBrowser(async (browser) => {
+        const documentGenerator = createDocumentGenerator(
+            browser,
+            requestAnalyzer
+        );
+        await documentGenerator.initialize();
+
+        const pageUrl = new URL(`${appUrl}/${generatePath}?id=${requestId}`)
+            .href;
+        const resultUrl = new URL(`${appUrl}/${resultPath}?id=${requestId}`)
+            .href;
+
+        const document = await documentGenerator.generateDocument(
+            appUrl,
+            pageUrl,
+            securityToken,
+            timezone,
+            useScreenMediaType,
+            waitForIdleNetwork
+        );
+
+        if (document === undefined)
+            throw new Error(`Failed to generate document. Document is empty`);
+
+        const documentSize = formatSize(document.length, {
+            minimumFractionDigits: 3,
+        });
+
+        logMessage(`Document size: ${documentSize}`);
+
+        if (enableMetrics) {
+            const metrics = await documentGenerator.getPageMetrics();
+            logMessage(`Page metrics: ${JSON.stringify(metrics)}`);
+
+            const requestStatistics = documentGenerator.getRequestStatistics();
+            logMessage(
+                `Request statistics: ${JSON.stringify(requestStatistics)}`
+            );
+        }
+
+        await moduleConnector.sendResult(resultUrl, document, securityToken);
+    }).catch((error) => {
+        logMessage(error.message, logLevel.error);
+        process.exit(1);
+    });
+
+    async function withBrowser(fn) {
+        const browser = createBrowser(chromePath);
+        await browser.initialize();
+
+        try {
+            await fn(browser).catch((error) => {
+                throw error;
+            });
+        } finally {
+            await browser.close();
+        }
+    }
+    //end
 
     res.status(200).send("Request received successfully");
 });
